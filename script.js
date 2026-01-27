5b8e19b2b6dd08713631393dcbddc40478b3a6b3
Valentin Garcia <vgarcia@frsn.utn.edu.ar>
2026-01-27 00:28:40 -0300

    Remove dark mode toggle event listener
    Removed dark mode toggle functionality.

--- script.js
+++ script.js
@@ -27,11 +27,6 @@ const firebaseConfig = {
   appId: "1:764048708615:web:1287e135d38a3588b806a2"
 };
 
-const btnDark = document.getElementById("btnToggleDark");
-btnDark.addEventListener("click", ()=>{
-  document.body.classList.toggle("dark-mode");
-});
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
