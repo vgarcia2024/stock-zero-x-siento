32fd0080019c0260b8ab4e33bd8ef123f990d489
Valentin Garcia <vgarcia@frsn.utn.edu.ar>
2026-02-27 17:07:13 -0300

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+/* 🔹 CONFIG FIREBASE */
+const firebaseConfig = {
+  apiKey: "AIzaSyCwBuxmHQgaqJAac_WiMZkpZUMFVzONFkA",
+  authDomain: "zero-x-siento-stock.firebaseapp.com",
+  projectId: "zero-x-siento-stock",
+  storageBucket: "zero-x-siento-stock.firebasestorage.app",
+  messagingSenderId: "764048708615",
+  appId: "1:764048708615:web:1287e135d38a3588b806a2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
