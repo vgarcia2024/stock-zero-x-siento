1200cb6dae718c3a7f98a7d186635f4d1f4aa564
Valentin Garcia <vgarcia@frsn.utn.edu.ar>
2026-03-18 20:51:18 -0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 /* 🔹 CONFIG FIREBASE */
 const firebaseConfig = {
@@ -12,9 +13,9 @@ const firebaseConfig = {
   appId: "1:764048708615:web:1287e135d38a3588b806a2"
 };
 
-const app = initializeApp(firebaseConfig);
+const app     = initializeApp(firebaseConfig);
+const auth    = getAuth(app);
+const db      = getFirestore(app);
+const storage = getStorage(app);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export { app, auth, db, storage };
